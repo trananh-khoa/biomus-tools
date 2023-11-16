266eca67f5744e1c328366501cce9dabbf2c5c1c
Khoa Tran <trananh_khoa@outlook.com>
2023-11-16 17:10:27 -0500

    feat(animation): added page transitions

--- app.vue
+++ app.vue
@@ -15,4 +15,15 @@ html, body, #__nuxt {
 body {
   @apply antialiased bg-neutral-800/80 font-sans font-400 text-white
 }
+
+/* https://nuxt.com/docs/getting-started/transitions */
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>

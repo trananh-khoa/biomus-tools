f4ae22709d65880597e1f3a33920f44b9278a296
Khoa Tran <trananh_khoa@outlook.com>
2023-11-16 08:43:17 -0500

    feat(setup): added layouts and pages

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <!-- https://nuxt.com/docs/guide/directory-structure/layouts -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <main>
+    <slot />
+  </main>
+</template>
+
+<style>
+
+</style>

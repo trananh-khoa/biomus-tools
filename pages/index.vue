8980538cc1831cbe6a3d166ec5a9fb967407abde
Khoa Tran <trananh_khoa@outlook.com>
2023-11-16 19:14:14 -0500

    feat(home): centered introduction text

--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,13 @@
 </script>
 
 <template>
-  <div un-px-4>
-    <h1 class="text-rainbow-gradient" un-text="3xl" un-font-700 un-hue-rotate>
+  <div un-text-center un-mt-4>
+    <h1 class="text-rainbow-gradient" un-text="3xl" un-font-700 un-hue-rotate un-mb-4>
       Biomedical Ultrasound Tools
     </h1>
-    <p class="text-rainbow">
-      A collection of tools.
+    <p un-text-neutral-400>
+      A collection of tools built for the <span un-font-700>Biomedical Ultrasound Laboratory</span>
+      at <span un-font-700>Carleton University</span>.
     </p>
   </div>
 </template>
